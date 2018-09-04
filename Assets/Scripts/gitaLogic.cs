4d6394b2c8481f7a95287b402935465ba881dbe6
Delton Hulbert <hulbert.delton@yahoo.com>
2018-09-04 18:03:58 -0400

    sept. 4 push
    i dunno what these changes are but i trust git somehow

--- Assets/Scripts/gitaLogic.cs
+++ Assets/Scripts/gitaLogic.cs
@@ -8,17 +8,26 @@ public class GitaLogic : MonoBehaviour {
     [Range(0f, 650f)]
     public float maxMotorTorque;
     public float offsetMaximum = 14f;
-    public float CenterOfMassY = 1f;
-    public float CenterOfMassZ = 1.03f;
+
+    [Header("------------Torque Curve------------")]
+    [Range(0f, 1f)]
+    public float torqueRampUpPercentage = 0.2f;
+    [Range(0f, 2f)]
+    public float torqueRampUpValue = 1.2f;
+    [Range(0f, 1f)]
+    public float torqueMaintainPercentage = 0.2f;
+    [Range(0f, 1f)]
+    public float torqueMaintainValue = 1.0f;
+    
 
     [Header("------------Hookups------------")]
     public Transform wheelAxle;
     public WheelCollider leftWheel;
     public WheelCollider rightWheel;
-    public GameObject CenterMassSphere;
     public UiManager uiManager;
 
     public float speed;
+    //private float speedCalc;
     private Vector3 wheelsPosition;
     private Vector3 defaultAxlePosition;
     private Vector3 defaultWheelLPosition;
@@ -33,14 +42,16 @@ public class GitaLogic : MonoBehaviour {
         rigBody.centerOfMass = new Vector3(0, 1f, 1.03f);
         wheelsPosition = new Vector3(0, 0, 0);
 
+        torqueRampUpPercentage = 1 - torqueRampUpPercentage;
+        torqueMaintainPercentage = 1 - torqueMaintainPercentage;
+
         defaultAxlePosition = wheelAxle.transform.localPosition;
         defaultWheelLPosition = leftWheel.center;
         defaultWheelRPosition = rightWheel.center;
     }
     
     private void FixedUpdate() {
-        CenterMassSphere.transform.localPosition = rigBody.centerOfMass;
-        rigBody.centerOfMass = new Vector3(0, CenterOfMassY, CenterOfMassZ);
+        //speedCalc = uiManager.barMover / 720;
 
         if (wheelOffset > offsetMaximum - (0.2f * offsetMaximum) || wheelOffset < -(offsetMaximum - (0.2f * offsetMaximum))) {
             wheelsPosition.y = Mathf.Lerp(wheelsPosition.y, (Mathf.Abs(wheelOffset) - offsetMaximum - (0.2f * offsetMaximum)) / 4f, Time.deltaTime);
@@ -93,32 +104,43 @@ public class GitaLogic : MonoBehaviour {
         //TODO make a motion selection UI item that switches between movement systems
         //TODO make a "carrot" movement system, where gita chases a carrot
         //TODO make a "spline" movement system, where gia follows a set path
-        
-        float speedCalc = 0;
-        if (uiManager.barMover != 0) {
-            speedCalc = uiManager.barMover / 720;
-        } else speedCalc = 0;
-        
-        float motor = maxMotorTorque * Input.GetAxis("Vertical") * speedCalc;
+                
+        //float motor = maxMotorTorque * Input.GetAxis("Vertical") * speedCalc;
 
         if (Input.GetAxis("Horizontal") < 0.01f && Input.GetAxis("Horizontal") > -0.01f) {
             turnDirection = 0;
         }
-        
-        if (turnDirection != 0) {
-            motorL = maxMotorTorque * -turnDirection;
-            motorR = maxMotorTorque * turnDirection;
-        } else {
-            motorL = motor;
-            motorR = motor;
+
+        motorL = TorqueCurve();
+        motorR = TorqueCurve();
+
+        if (turnDirection < 0) {
+            motorR = Input.GetAxis("Horizontal") * TorqueCurve() * 0.2f;
+        }
+        if (turnDirection > 0) { 
+            motorL = -Input.GetAxis("Horizontal") * TorqueCurve() * 0.2f;
+        }
+
+        if (Input.GetAxis("Vertical") < .001 && Input.GetAxis("Vertical") > -.001) {
+            motorL = Input.GetAxis("Horizontal") * -maxMotorTorque;
+            motorR = Input.GetAxis("Horizontal") * maxMotorTorque;
+
         }
     }
 
-    private void TorqueCurve() {
+    private float TorqueCurve() {
+        float desiredSpeed = Input.GetAxis("Vertical") * (maxMotorTorque);
+
+        if (speed < (desiredSpeed / 50.0f) * torqueRampUpPercentage) {
+            return Input.GetAxis("Vertical") * maxMotorTorque * torqueRampUpValue;
+        } else if (speed > (desiredSpeed / 50.0f) * torqueMaintainPercentage) {
+            return desiredSpeed * torqueMaintainValue;
+        }
+
+        return 0f;
         //TODO make a torque curve that applies a lot of torque when at a standstill, then reduces to maintain speed when desired speed is reached.
-            //make sure to delete the mouse control from speedChoose. Maybe delete the whole function
             //allow user to determine the "curve" with some interface values: value (vertical) and location (horizontal)
             // when no input is given, maintain current speed
-            // add SPACEBAR to stop. reduce torque to 0
+            // add SPACEBAR to stop. reduce torque to 0 when nop other keys but space are pressed
     }
 }
